2a0e98bb7d2e49f82580e2e300fbf19a13b5d152
Denis Suvolokin <budden@list.ru>
2018-03-16 18:35:41 +0400

    fix bug with duplicate histories with one meter when seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
 Meter.create(value_total:  1, feature: :gas, placed_at: :kitch )
 Meter.create(value_total:  2, feature: :hwater, placed_at: :bath )
 Meter.create(value_total:  3, feature: :cwater, placed_at: :bath )
 Meter.create(value_total:  4, feature: :electr, placed_at: :hall )
 
-Meter.all.each{|meter| History.create(meter_id: meter.id)}
+#to protect from duplicate of history related with each meter
+@histories=History.all
+Meter.all.each do |meter| 
+  history=@histories.where(meter_id: meter.id)
+  p history
+  unless history
+    History.create(meter_id: meter.id)
+  end
+end
